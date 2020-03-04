48e73852ee46fd5878fc0472477ace7e30182a25
Kate LaFrance <Kate.LaFrance@oit.edu>
2020-03-04 14:44:00 -0800

    added folder for Space invaders half

---
+++ LuaInvaders/main.lua
@@ -0,0 +1,9 @@
+
+
+
+function love.draw ()
+	--- Set color of box---
+	love.graphics.setColor(0, 0, 255)
+	--- Drawing the box that moves ---
+	love.graphics.rectangle("fill", player.x, 400, 80, 20)
+end
