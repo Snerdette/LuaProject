6b748158450c2786ab0eafcb85a0a66b7ea66364
Kate LaFrance <Kate.LaFrance@oit.edu>
2020-03-12 15:15:43 -0700

    thin verticle slice done, not tested

--- LuaInvaders/main.lua
+++ LuaInvaders/main.lua
@@ -1,17 +1,51 @@
---- Global Variables Set Up ---
+--- Global Variables Set Up ----------
 love.graphics.setDefaultFilter('nearest', 'nearest')
 enemy = {}
 enemies_controller = {}
 enemies_controller.enemies = {}
 enemies_controller.image = love.graphics.newImage('enemy.png')
 enemies_controller.hit_sound = love.audio.newSource("hit.wav", "static")
+game_music = love.audio.newSource('GameMusic.mp3', 'static')
 
---- Collision of Bullets and Enemies---
+particle_systems = {}
+particle_systems.img = love.graphics.newImage('particle.png')
+
+function particle_systems:spawn(x, y)
+    local ps = {}
+    ps.x = x
+    ps.y = y
+    ps.ps = love.graphics.newParticleSystem(particle_systems.img, 32)
+    ps.ps:setParticleLifetime(2, 4)
+    ps.ps:setEmissionRate(5)
+    ps.ps:setSizeVariation(1)
+    ps.ps:setLinearAcceleration(-20, -20, 20, 20)
+    ps.ps:setColors(100, 255, 100, 255, 0, 255, 0, 255)
+    table.insert(particle_systems.list, ps)
+end
+
+function particle_systems:draw()
+    for _, v in pairs(particle_systems.list) do
+        love.graphics.draw(v.ps, v.x, v.y )
+    end
+end
+
+function particle_systems:update(dt) 
+    for _, v in pairs(particle_systems.list) do
+        v.pc:update(dt)
+    end
+end
+
+
+function particle_systems:cleanup()
+---- TODO : clean up old particles -----
+end
+--- Collision of Bullets and Enemies-------------------------------------------------  CHECK COLLISION  --------------------------------------
 function checkCollisions(enemies, bullets)
     for _, e in ipairs(enemies) do
         for _, b in pairs(bullets) do
             if b.y <= e.y + e.height and b.x > e.x and b.x < e.x + e.width then
                 -- Destroy Enemy ---
+                particle_systems:spawn(e.x, e.y)
                 print("Collision!")
                 enemies_controller.hit_sound:play()
                 table.remove(enemies, i)             
@@ -23,7 +57,11 @@ end
 
 
 --- Enemy set up because load is called after -- 
+-------------------------------------------------------------------------------------  LOAD  -------------------------------------------------
 function love.load()
+--- Game Music Looping  -----
+    game_music:setLooping(true)
+    love.audio.play(game_music)
 --- Game Over & Game Win Variables ---
     game_over = false
     game_win = false
@@ -56,8 +94,8 @@ function love.load()
     end
     
 end
-
---- Spawning Each Enemy ---
+   
+--- Spawning Each Enemy -------------------------------------------------------------  SPAWN ENEMY  ------------------------------------
 function enemies_controller:spawnEnemy(x, y)
     enemy = {}
     enemy.x = x
@@ -71,7 +109,7 @@ function enemies_controller:spawnEnemy(x, y)
     table.insert(enemies_controller.enemies, enemy)
 end
 
---- Shooting Each Bullet ---
+--- Shooting Each Bullet ------------------------------------------------------------  ENEMY:FIRE  ---------------------------------------
 function enemy:fire ()
     if self.cooldown <= 0 then
         self.cooldown = 20
@@ -82,7 +120,7 @@ function enemy:fire ()
     end
 end
 
---- Constantly updating Function ---
+--- Constantly updating Function ----------------------------------------------------  UPDATE  ------------------------------------------
 function love.update(dt) --- dt =  delta time variable
     
 --- Bullet cooldown decrement---
@@ -129,17 +167,18 @@ end
 function love.draw ()
 --- Setting Scale ---
     love.graphics.scale(3)
+    --- Drawing background ---
+    love.graphics.draw(background_image)
 --- Drawing Game Over Screen if True ---
     if game_over then
-        love.graphics.draw("Game Over")
+        love.graphics.print("Game Over")
         --- Ends game ---
         return
     elseif game_win then
-        love.graphics.draw("Game Won!")
+        love.graphics.print("Game Won!")
         --- Lets you keep shooting ---
     end
---- Drawing background ---
-    love.graphics.draw(background_image)
+
 --- Drawing the Player ---
     love.graphics.setColor(255, 255, 255)
     love.graphics.draw(player.image, player.x, player.y)
